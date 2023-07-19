15f994c5c20b90db60ab247377bd03814618c1c7
Yadhu <yadhukrishnanas@gmail.com>
2023-07-19 15:54:56 +0530

    commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./Todo.css";
+import { useState } from "react";
 
 function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos,setTodos] = useState([])
+
+  const addTodo = () =>{
+    setTodos([...todos,todo])
+    console.log(todos);
+  }
+
   return (
-    <div>Todo</div>
-  )
+    <div className="container">
+      <h2>TODO APP</h2>
+      <form className="form-group">
+        <input
+          type="text"
+          value={todo}
+          placeholder="Add Task"
+          className="form-control"
+          onChange={(event) => setTodo(event.target.value)}
+        />
+        <button onClick={addTodo}>ADD</button>
+      </form>
+      <div className="list">
+        <ul>
+          <li className="form-control">First</li>
+          <li className="form-control">Second</li>
+          <li className="form-control">Third</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
